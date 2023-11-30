8fae1948956c48582c8649db0b0b2dfc849f29b4
Sebastian Fleischanderl <fleiseb@gmail.com>
2023-11-30 09:14:48 +0100

    added override

--- SudokuSolver/SudokuBoard.cs
+++ SudokuSolver/SudokuBoard.cs
@@ -9,7 +9,7 @@ namespace SudokuSolver {
     private int[,] board;
     private bool[,] fixedNumbers;
 
-    public toString(){
+    public override toString(){
       for (int i = 0; i < 9; i++){
         for (int j = 0; j < 9; j++){
             Console.Write(board[i, j] + " ");
